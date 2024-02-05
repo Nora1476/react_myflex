99340e7f12c580625a37de3c025055e1d1efd695
Bonghui(Nora) <74698994+Nora1476@users.noreply.github.com>
2024-02-05 15:44:54 +0900

    240205

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, RouterProvider } from "react-router-dom";
 import Router from "./Router";
 import Header from "./Components/Header";
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Myflex</title>
+      </Helmet>
       <RouterProvider router={Router}></RouterProvider>;
       <BrowserRouter>
         <Header />
